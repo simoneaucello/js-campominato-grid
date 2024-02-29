f417ddfd871a54bb07b123fe2a01a09b1b5f04d9
simoneaucello <simoneaucello@gmail.com>
2024-02-29 16:19:36 +0100

    [add] clicked square

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ reset();
 for (let i = 1; i <= 100; i++) {
   const square = mySquare(i);
   gridContainer.append(square);
-  console.log(square);
 }
 
 
@@ -21,7 +20,18 @@ for (let i = 1; i <= 100; i++) {
 function mySquare(index){
   const sq = document.createElement('div');
   sq.className = 'square'; 
-  
+  // creo una proprietà custom per sq e la chiamo _sqID 
+  sq._sqID = '';
+
+  sq.addEventListener('click', function(){
+
+    this.innerHTML = (!this.classList.add('clicked'))
+                      ? this.innerHTML = this._sqID
+                      : this.innerHTML = '';
+
+  console.log(index);
+  })
+
   return sq
 }
 
