6123e29a44efec0aa63847c0e04ab40b25e3a726
simoneaucello <simoneaucello@gmail.com>
2024-02-29 18:39:55 +0100

    [add] bonus

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
+
 const gridContainer = document.querySelector('.grid-container');
-
-reset();
-
-// genero 100 square con un ciclo for
-
-for (let i = 1; i <= 100; i++) {
-  const square = mySquare(i);
-  gridContainer.append(square);
-}
+const btnGenerate = document.querySelector('.btn-generate');
+const difficulty = document.getElementById('choose');
 
 
-
+btnGenerate.addEventListener('click', function (){
+  let difficultyValue = difficulty.value;
+  
+  if(difficultyValue === 'easy'){
+    easy();
+  } else if(difficultyValue === 'medium'){
+    medium();
+  } else if(difficultyValue === 'hard'){
+    hard();
+  }
+})
 
 
 
 ///// FUNCTION /////
+
+function easy(){
+  reset();
+  // genero 100 square con un ciclo for
+for (let i = 1; i <= 100; i++) {
+  const square = mySquare(i);
+  gridContainer.append(square);
+}
+}
+
+function medium(){
+  reset();
+  // genero 81 square con un ciclo for
+for (let i = 1; i <= 81; i++) {
+  const square = mySquare(i);
+  gridContainer.append(square);
+  square.classList.add('medium');
+}
+}
+
+function hard(){
+  reset();
+  // genero 49 square con un ciclo for
+for (let i = 1; i <= 49; i++) {
+  const square = mySquare(i);
+  gridContainer.append(square);
+  square.classList.add('hard');
+}
+}
 
 
 function mySquare(index){
